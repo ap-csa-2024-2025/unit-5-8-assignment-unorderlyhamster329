e9b880b94a6e0ca4f423b32520a733f8e861f3db
unorderlyhamster329 <118472366+unorderlyhamster329@users.noreply.github.com>
2025-01-08 19:04:26 +0000

    sadada

--- Dog.java
+++ Dog.java
@@ -5,12 +5,37 @@ public class Dog
   private static int numDogs = 0;
 
   // Non-default Dog constructor
+public Dog(String name){
+  this.name = name;
+  numDogs++;
+  this.ID = numDogs * numDogs;
 
+}
   // Default Dog constructor
 
   // Setters - instance variables only
+public void setName(String iName) {
+  name = iName;
+}
 
+public void setID(int iID){
+  ID = iID;
+}
+
+public void setNumDogs(int iNumDogs) {
+  numDogs = iNumDogs;
+}
   // Getters - instance variables only
+public String getName() {
+  return name;
+}
 
+public int getID() {
+  return ID;
+}
+
+public int getNumDogs() {
+  return numDogs;
+}
   // method getNumDogs
 }
